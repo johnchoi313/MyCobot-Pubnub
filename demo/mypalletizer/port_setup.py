5237133e82c9ccd8d0618c40bdff18ba4194ae94
John Choi <johnchoi313@gmail.com>
2023-03-19 15:50:47 -0400

    Update port_setup.py

--- demo/mypalletizer/port_setup.py
+++ demo/mypalletizer/port_setup.py
@@ -4,35 +4,7 @@ import serial.tools.list_ports
 from pymycobot.mypalletizer import MyPalletizer
 
 def setup():
-    print("")
-
-    plist = list(serial.tools.list_ports.comports())
-    idx = 1
-    for port in plist:
-        print("{} : {}".format(idx, port))
-        idx += 1
-
-    _in = input("\nPlease input 1 - {} to choice:".format(idx - 1))
-    port = str(plist[int(_in) - 1]).split(" - ")[0].strip()
-    print(port)
-    print("")
-
-    baud = 115200
-    _baud = input("Please input baud(default:115200):")
-    try:
-        baud = int(_baud)
-    except Exception:
-        pass
-    print(baud)
-    print("")
-
-    DEBUG = False
-    f = input("Wether DEBUG mode[Y/n]:")
-    if f in ["y", "Y", "yes", "Yes"]:
-        DEBUG = True
-    
-    # mc = MyCobot(port, debug=True)
-    mc = MyPalletizer(1, 1000000, debug=DEBUG)
+    mc = MyPalletizer(1, 1000000, debug=True)
 
     return mc
 
